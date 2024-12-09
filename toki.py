2368225ccacd674b6d031ef762eeb3c7b2e062e8
Kiaan <kiaan24@tutanota.com>
2024-12-09 17:22:42 +0000

    Create toki.py

---
+++ toki.py
@@ -0,0 +1,19 @@
+print("toki! nimi sina seme?")
+nimi = input()
+print(nimi,". ona li nimi pona!")
+print("kule li pona mute tawa sina li seme?")
+kule = input()
+if kule == "loje":
+  print("mi ken toki ala")
+elif kule == "loje jelo":
+  print("a, kule ni li pona tawa mi!")
+elif kule == "jelo":
+  print("a! ni li jaki")
+elif kule == "laso kasi":
+  print("a, laso kasi - kule pi soko jaki - ona li kule ike!")
+elif kule == "laso":
+  print("a! ni li jaki")
+elif kule == "loje laso":
+  print("a! ni li jaki")
+else:
+  print("a!", kule,"li pona mute tawa mi(!)")
